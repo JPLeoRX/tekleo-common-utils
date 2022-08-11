c2aba7beaaa75eee94ccc8b4a735672136d35e59
leo <leo.ertuna@gmail.com>
2022-08-11 15:24:57 +0300

    Test heic with exif

--- tekleo_common_utils/test/test_utils_image.py
+++ tekleo_common_utils/test/test_utils_image.py
@@ -8,8 +8,16 @@ u = UtilsImage()
 # Test download
 u.download_image_pil('https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/2006-2009_Honda_Civic_VTi-L_sedan_01.jpg/640px-2006-2009_Honda_Civic_VTi-L_sedan_01.jpg')
 
-# Test Exif
+# Test Exif (JPG)
 image_pil = u.open_image_pil("IMG_9798.JPG")
-image_without_exif_pil = u.clear_exif_data(image_pil)
 print(image_pil.getexif())
+image_without_exif_pil = u.clear_exif_data(image_pil)
 print(image_without_exif_pil.getexif())
+image_without_exif_pil.save("IMG_9798_NO_EXIF.JPG")
+
+# Test Exif (HEIC)
+image_pil = u.open_image_pil("IMG_8629.HEIC")
+print(image_pil.getexif())
+image_without_exif_pil = u.clear_exif_data(image_pil)
+print(image_without_exif_pil.getexif())
+image_without_exif_pil.save("IMG_8629_NO_EXIF.HEIC")
