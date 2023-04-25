a321999a5af3a44601e07ff907d7fbeb5cd68708
leo <leo.ertuna@gmail.com>
2023-04-25 22:10:02 +0300

    Point math

--- tekleo_common_utils/utils_math.py
+++ tekleo_common_utils/utils_math.py
@@ -1,5 +1,7 @@
+import math
 from typing import List
 from injectable import injectable
+from tekleo_common_message_protocol import PointRelative
 
 
 @injectable
@@ -32,3 +34,36 @@ class UtilsMath:
                 return self.reduce_list_int(values, reduction_delta)
 
         return values
+
+    def points_add(self, point_a: PointRelative, point_b: PointRelative) -> PointRelative:
+        return PointRelative(
+            point_a.x + point_b.x,
+            point_a.y + point_b.y
+        )
+
+    def points_subtract(self, point_a: PointRelative, point_b: PointRelative) -> PointRelative:
+        return PointRelative(
+            point_a.x - point_b.x,
+            point_a.y - point_b.y
+        )
+
+    def points_multiply(self, point_a: PointRelative, point_b: PointRelative) -> PointRelative:
+        return PointRelative(
+            point_a.x * point_b.x - point_a.y * point_b.y,
+            point_a.x * point_b.y + point_a.y * point_b.x
+        )
+
+    def point_multiply(self, point_a: PointRelative, c: float) -> PointRelative:
+        return PointRelative(
+            point_a.x * c,
+            point_a.y * c
+        )
+
+    def point_rotate(self, point_a: PointRelative, point_center: PointRelative, angle: float) -> PointRelative:
+        angle_radians = math.radians(angle)
+        angle_cos = math.cos(angle_radians)
+        angle_sin = math.sin(angle_radians)
+        point_a_shifted = self.points_subtract(point_a, point_center)
+        point_a_rotated = self.points_multiply(point_a_shifted, PointRelative(angle_cos, angle_sin))
+        point_a_rotated_back = self.points_add(point_a_rotated, point_center)
+        return point_a_rotated_back
