5394794710cdd08d4de89fd58b7a423afdd2c5e8
leo <leo.ertuna@gmail.com>
2023-05-10 21:57:37 +0300

    Method to draw rounded rectangles

--- tekleo_common_utils/utils_opencv.py
+++ tekleo_common_utils/utils_opencv.py
@@ -14,6 +14,8 @@ class UtilsOpencv:
     def __init__(self, utils_math: Autowired(UtilsMath)):
         self.utils_math = utils_math
 
+    # Dimensions
+    #-------------------------------------------------------------------------------------------------------------------
     def get_dimensions_hw(self, image_cv: ndarray) -> (int, int):
         h, w = image_cv.shape[:2]
         return h, w
@@ -33,6 +35,7 @@ class UtilsOpencv:
             return image_cv.shape[-1]
         else:
             raise ValueError("Weird image with ndim=" + str(image_cv.ndim))
+    #-------------------------------------------------------------------------------------------------------------------
 
     def are_images_equal(self, image_cv_a: ndarray, image_cv_b: ndarray) -> bool:
         if image_cv_a.shape == image_cv_b.shape:
@@ -182,6 +185,8 @@ class UtilsOpencv:
                 max_BGR = BGR
     
         return max_BGR
+
+
 
     # Image transformations
     #-------------------------------------------------------------------------------------------------------------------
@@ -399,6 +404,7 @@ class UtilsOpencv:
     #-------------------------------------------------------------------------------------------------------------------
 
 
+
     def edge_detection(self, image_cv: ndarray) -> List[List[PointPixel]]:
         image_gray_cv = self.convert_to_grayscale(image_cv)
         image_blurred_cv = self.blur_gaussian(image_gray_cv, 3, 3)
@@ -427,3 +433,57 @@ class UtilsOpencv:
             contours_as_points.append(points)
 
         return contours_as_points
+
+
+
+    # Drawing
+    #-------------------------------------------------------------------------------------------------------------------
+    def draw_rectangle_rounded(self, image_cv: ndarray, top_left: Tuple[int, int], bottom_right: Tuple[int, int], radius_scale: float = 1.0, color: List[int] = [255, 255, 255], thickness: float = 1.0, line_type: int = cv2.LINE_AA) -> ndarray:
+        # Make sure we make a copy
+        image_cv = image_cv.copy()
+
+        #  corners:
+        #  p1 - p2
+        #  |     |
+        #  p4 - p3
+        p1 = top_left
+        p2 = (bottom_right[0], top_left[1])
+        p3 = (bottom_right[0], bottom_right[1])
+        p4 = (top_left[0], bottom_right[1])
+
+        # Calculate corners
+        height = abs(bottom_right[1] - top_left[1])
+        corner_radius = int(radius_scale * (height / 2))
+
+        if thickness < 0:
+            #big rect
+            top_left_main_rect = (int(p1[0] + corner_radius), int(p1[1]))
+            bottom_right_main_rect = (int(p3[0] - corner_radius), int(p3[1]))
+
+            top_left_rect_left = (p1[0], p1[1] + corner_radius)
+            bottom_right_rect_left = (p4[0] + corner_radius, p4[1] - corner_radius)
+
+            top_left_rect_right = (p2[0] - corner_radius, p2[1] + corner_radius)
+            bottom_right_rect_right = (p3[0], p3[1] - corner_radius)
+
+            all_rects = [
+                [top_left_main_rect, bottom_right_main_rect],
+                [top_left_rect_left, bottom_right_rect_left],
+                [top_left_rect_right, bottom_right_rect_right]]
+
+            [cv2.rectangle(image_cv, rect[0], rect[1], color, thickness) for rect in all_rects]
+
+        # draw straight lines
+        cv2.line(image_cv, (p1[0] + corner_radius, p1[1]), (p2[0] - corner_radius, p2[1]), color, abs(thickness), line_type)
+        cv2.line(image_cv, (p2[0], p2[1] + corner_radius), (p3[0], p3[1] - corner_radius), color, abs(thickness), line_type)
+        cv2.line(image_cv, (p3[0] - corner_radius, p4[1]), (p4[0] + corner_radius, p3[1]), color, abs(thickness), line_type)
+        cv2.line(image_cv, (p4[0], p4[1] - corner_radius), (p1[0], p1[1] + corner_radius), color, abs(thickness), line_type)
+
+        # draw arcs
+        cv2.ellipse(image_cv, (p1[0] + corner_radius, p1[1] + corner_radius), (corner_radius, corner_radius), 180.0, 0, 90, color, thickness, line_type)
+        cv2.ellipse(image_cv, (p2[0] - corner_radius, p2[1] + corner_radius), (corner_radius, corner_radius), 270.0, 0, 90, color, thickness, line_type)
+        cv2.ellipse(image_cv, (p3[0] - corner_radius, p3[1] - corner_radius), (corner_radius, corner_radius), 0.0, 0, 90, color, thickness, line_type)
+        cv2.ellipse(image_cv, (p4[0] + corner_radius, p4[1] - corner_radius), (corner_radius, corner_radius), 90.0, 0, 90, color, thickness, line_type)
+
+        return image_cv
+    #-------------------------------------------------------------------------------------------------------------------
