c2ffba63af41e6ddeea389926c7bd644b520a1ba
jpleorx <leo.ertuna@gmail.com>
2024-07-14 10:09:02 +0300

    Bump up the version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as readme_file:
 
 setup_args = dict(
     name='tekleo-common-utils',
-    version='0.0.2.7',
+    version='0.0.2.8',
     description="",
     keywords=[],
     long_description=readme_text,
